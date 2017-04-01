928ed0e87a1650f11cb2c79406aba785cccc31ab
Charleswils <asikpo@uwindsor.ca>
2017-04-01 04:18:46 -0400

    Create registerUser.php

---
+++ registerUser.php
@@ -0,0 +1,31 @@
+<?php
+include("connection.php");
+global $connection;
+
+	$strUsername = $_POST["inputUserName"];
+	$strPassword = $_POST["inputNewPassword"];
+	$strConfirmPwd = $_POST["ConfirmPassword"];//var_dump($_POST);
+	if($_POST){
+		if(empty($strConfirmPwd) || empty($strPassword) || empty($strUsername)){
+			header("Location: http://asikpo.myweb.cs.uwindsor.ca/60270/Project/signup.php?blnError=1");
+		}
+		else if($strConfirmPwd != $strPassword){
+			header("Location: http://asikpo.myweb.cs.uwindsor.ca/60270/Project/signup.php?blnError=2");
+		}
+		else{
+			$strSQL = "INSERT INTO asikpo_270Project.tblUsers
+						(strUserName, strPassword)
+						VALUES ('$strUsername', '$strPassword')";
+
+			$rsResult = mysqli_query($connection, $strSQL);
+			//echo "Query Used: ". $strSQL;
+			$intUserID = mysqli_insert_id($connection);
+			if(!$rsResult){
+				header("Location: http://asikpo.myweb.cs.uwindsor.ca/60270/Project/signup.php?blnError=3");
+			}
+			else{
+				header("Location: http://asikpo.myweb.cs.uwindsor.ca/60270/Project/homePage.php?UserID=".$intUserID);
+			}				
+		}
+	}
+?>
